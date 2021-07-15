35032c920e356dac21919de255785ba67653ddcf
matt.macarty@gmail.com <matt.macarty@gmail.com>
2021-07-15 11:47:39 -0400

    class files

--- program_files/0707_reaction.py
+++ program_files/0707_reaction.py
@@ -1,10 +1,19 @@
 # reaction time tester
+"""
+Improvements added:
+1. Add randomness to prompt
+2. Use time and sleep
+3. give the user 5 tries
+4. display reaction time with 3 decimals
+5. after fifth try display average time
+"""
 
-import time
+from time import time, sleep
+from random import random
 
 print("Welcome to the reaction time tester!")
-start_time = time.time()
+start_time = time()
 print("Hit the enter key as fast as you can.")
 input()
-elapsed_time = time.time() - start_time
+elapsed_time = time() - start_time
 print("Wow, that was fast. It took " + str(elapsed_time) + " seconds")
